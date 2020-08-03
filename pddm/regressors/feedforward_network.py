a01c4ab48615bd38634edf152de876cd557b4835
Agarwal <nikhil.agarwal@intel.com>
2020-08-02 19:19:13 -0700

    added histogram plots for weight bias grad

--- pddm/regressors/feedforward_network.py
+++ pddm/regressors/feedforward_network.py
@@ -56,8 +56,8 @@ class NetModel(torch.nn.Module):
 
     def init_weights_bias(self, m):
         if isinstance(m, torch.nn.Linear):
-            torch.nn.init.xavier_uniform_(m.weight)
-            # torch.nn.init.zeros_(m.weight)
+            # torch.nn.init.xavier_uniform_(m.weight)
+            torch.nn.init.zeros_(m.weight)
             torch.nn.init.zeros_(m.bias)
 
 
@@ -76,13 +76,13 @@ def perform_experiment(exp_type, batch_size, n_iter, tag):
         input = np.arange(1, 51, dtype=np.float32)
         target = [x ** 2 for x in input]
         # input1 = input.reshape((batch_size, 1))
-        input = torch.from_numpy(input)
+        input = torch.from_numpy(input).reshape((50,1))
         # input = torch.flatten(input2, start_dim=1)
         target = np.asarray(target, dtype=np.float32)
-        target = torch.from_numpy(target)
+        target = torch.from_numpy(target).reshape((50,1))
     else:
-        input = torch.zeros((50, ))
-        target = torch.ones((50, ))
+        input = torch.zeros((50, 1))
+        target = torch.ones((50, 1))
 
     # if exp_type == 1:
     #     input = np.arange(1, 500 + 1, dtype=np.float32).reshape(-1,1)
@@ -95,42 +95,75 @@ def perform_experiment(exp_type, batch_size, n_iter, tag):
     dataset = Dataset(input, target)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
 
-    opt = torch.optim.Adam(net.parameters(), lr=0.1)
-    loss = torch.nn.MSELoss()
+    opt = torch.optim.Adam(net.parameters(), lr=0.05)
+    # loss = torch.nn.MSELoss()
+    loss = torch.nn.L1Loss()
     # print('input: ', input)
     for i in range(n_iter):
-
+        counter = 0
+        losses = []
+        print('--------iter-----', i)
         for input, target in dataloader:
+            counter += 1
             curr_output = net.forward(input)
+            # print('--------counter-----', counter)
             opt.zero_grad()
+            # if i !=0:
+                # print('---gradient values init to zero----')
+                # print_net_grads(net)
             # this_mse = torch.mean(
             #     torch.pow(target - curr_output, 2))
             this_mse = loss(curr_output, target)
-            writer.add_scalar('loss '+tag, this_mse, i)
-            writer.flush()
+
             # print('-------------------------------------')
 
             # print('loss: ', this_mse)
             # print('output:', curr_output)
             # print('target:', target)
-            print('--------iter-----', i)
-            print('---weight bias values ----')
-            print_net_weights(net)
+
+            # print('---weight bias values ----')
+            # plot_net_weights(writer, net, 'model/batch/weights', i, batch=counter)
             # print(net.state_dict().keys())
             this_mse.backward()
-            print('---gradient values ----')
-            print_net_grads(net)
+            # print('---gradient values ----')
+            # plot_net_grads(writer, net, 'model/batch/grads', i , batch=counter)
             # plot_grad_flow(net.named_parameters())
             opt.step()
-            print('---updated weight bias values ----')
-            print_net_weights(net)
-        print('iter: ', i)
+            losses.append(this_mse.detach())
+            # print('---updated weight bias values ----')
+            # print_net_weights(net)
+        writer.add_scalar('loss ', sum(losses)/len(losses), i)
+        # print(net.state_dict().keys())
+        writer.add_histogram('layer_in/weight ', net.network.layer_in.weight, i)
+        writer.add_histogram('layer_in/bias ', net.network.layer_in.bias, i)
+        writer.add_histogram('layer_in/grad ', net.network.layer_in.weight.grad, i)
+        writer.add_histogram('layer_1/weight ', net.network.layer_1.weight, i)
+        writer.add_histogram('layer_1/bias ', net.network.layer_1.bias, i)
+        writer.add_histogram('layer_1/grad ', net.network.layer_1.weight.grad, i)
+        writer.add_histogram('layer_out/weight ', net.network.layer_out.weight, i)
+        writer.add_histogram('layer_out/bias ', net.network.layer_out.bias, i)
+        writer.add_histogram('layer_out/grad ', net.network.layer_out.weight.grad, i)
+        writer.flush()
+        # plot_net_weights(writer, net, 'model/iter/weights', i)
+        # plot_net_grads(writer, net, 'model/iter/grads', i)
+        # print('iter: ', i)
 
 def print_net_weights(net):
     vals = dict()
     for n, p in net.named_parameters():
-            vals[n] = (p.data.abs().mean(), p.data.abs().max())
-            print(n, vals)
+        vals[n] = (p.data.abs().mean(), p.data.abs().max())
+        print(n, vals)
+
+def plot_net_weights(writer, net, tag_, i, batch=None):
+    vals = dict()
+    b = ''
+    if batch:
+        b = 'batch_'+str(batch)
+    for n, p in net.named_parameters():
+        if 'bias' not in n:
+            vals[str(n)+b + '_mean'] = p.data.abs().mean()
+            # vals[str(n)+b + '_max'] = p.data.abs().max()
+    writer.add_scalars(tag_, vals, i)
 
 def print_net_grads(net):
     grads = dict()
@@ -138,36 +171,49 @@ def print_net_grads(net):
             grads[n] = (p.grad.abs().mean(), p.grad.abs().max())
             print(n, grads)
 
+def plot_net_grads(writer, net, tag_, i , batch=None):
+    grads = dict()
+    b =''
+    if batch:
+        b = 'batch_' + str(batch)
+    for n, p in net.named_parameters():
+        if 'bias' not in n:
+            grads[str(n)+b + '_mean'] = p.grad.abs().mean()
+            # grads[str(n)+b + '_max'] = p.grad.abs().max()
+    writer.add_scalars(tag_, grads, i)
+            # grads[n] = (p.grad.abs().mean(), p.grad.abs().max())
+            # print(n, grads)
 
-def plot_grad_flow(named_parameters):
-    '''Plots the gradients flowing through different layers in the net during training.
-    Can be used for checking for possible gradient vanishing / exploding problems.
 
-    Usage: Plug this function in Trainer class after loss.backwards() as
-    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
-    ave_grads = []
-    max_grads = []
-    layers = []
-    for n, p in named_parameters:
-        if (p.requires_grad) and ("bias" not in n):
-            layers.append(n)
-            ave_grads.append(p.grad.abs().mean())
-            max_grads.append(p.grad.abs().max())
-    # print('Max_grad: ', max_grads)
-    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
-    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
-    plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
-    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
-    plt.xlim(left=0, right=len(ave_grads))
-    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
-    plt.xlabel("Layers")
-    plt.ylabel("average gradient")
-    plt.title("Gradient flow")
-    plt.grid(True)
-    plt.legend([Line2D([0], [0], color="c", lw=4),
-                Line2D([0], [0], color="b", lw=4),
-                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
-    plt.show()
+# def plot_grad_flow(named_parameters):
+#     '''Plots the gradients flowing through different layers in the net during training.
+#     Can be used for checking for possible gradient vanishing / exploding problems.
+#
+#     Usage: Plug this function in Trainer class after loss.backwards() as
+#     "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+#     ave_grads = []
+#     max_grads = []
+#     layers = []
+#     for n, p in named_parameters:
+#         if (p.requires_grad) and ("bias" not in n):
+#             layers.append(n)
+#             ave_grads.append(p.grad.abs().mean())
+#             max_grads.append(p.grad.abs().max())
+#     # print('Max_grad: ', max_grads)
+#     plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+#     plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+#     plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
+#     plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+#     plt.xlim(left=0, right=len(ave_grads))
+#     plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+#     plt.xlabel("Layers")
+#     plt.ylabel("average gradient")
+#     plt.title("Gradient flow")
+#     plt.grid(True)
+#     plt.legend([Line2D([0], [0], color="c", lw=4),
+#                 Line2D([0], [0], color="b", lw=4),
+#                 Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+#     plt.show()
 
 class Dataset(torch.utils.data.Dataset):
 
@@ -184,11 +230,11 @@ class Dataset(torch.utils.data.Dataset):
 
 
 if __name__ == "__main__":
-    writer = SummaryWriter('test/run_dataLoader_11', flush_secs=2)
+    writer = SummaryWriter('test/run_dataLoader_31', flush_secs=2)
     net = feedforward_network(1, 1, 2, 500)
-    batch_size = 5
-    n_iter = 10
-    perform_experiment(1, 1, n_iter, tag='square_func_batch_size_1_trial11')
+    batch_size = 10
+    n_iter = 100
+    perform_experiment(1, batch_size, n_iter, tag='square_func_batch_size_1_trial')
 
     # perform_experiment(0, 1, n_iter, tag='zeros_ones_batch_size_1_trial1')
     # perform_experiment(0, 50, n_iter, tag='zeros_ones_batch_size_50')
